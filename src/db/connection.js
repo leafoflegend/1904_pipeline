4488120adb2ad7f441a5ea1f05487d27c0b8864f
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-07-29 19:51:41 -0400

    Add database on before_script pt 2

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/1904-pipeline', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/1904pipeline', {
   logging: false,
 });
 
